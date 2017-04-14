48bfd9964d620c8f3981594ffb0993732ced6d83
= <jaafersheriff@gmail.com>
2017-04-14 12:44:26 -0700

    new scripts

---
+++ run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+import commands
+
+def runLine(command):
+   return subprocess.call(command, shell=True)
+
+runLine("clear")
+runLine("echo -n Compiling...")
+if (runLine("g++ src/*.cpp -I/cygdrive/c/Users/jaafe/Documents/Code/libs/glm-0.9.8.4/ -o build/raytrace > compileOut")) == 0:
+    runLine("echo done")
+    args = "";
+    for i in range (1, len(sys.argv)):
+        args += sys.argv[i] + " ";
+    runLine("./build/raytrace " + args);
+else:
+    runLine("echo ERROR!");
+    runLine("cat compileOut");
+runLine("rm compileOut");
