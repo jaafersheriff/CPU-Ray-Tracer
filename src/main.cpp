e9fcd11683f76cbbc1b9633619caad5450aaf7ba
= <jaafersheriff@gmail.com>
2017-04-22 00:28:10 -0700

    new arg storing process - TODO: enum

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,52 @@
 #include <vector>	// vectors
 #include <iomanip>	//set precision
 
-#include "Renderer.h"
-#include "Loader.h"
-#include "Scene.h"
+#include "Renderer.hpp"
+#include "Loader.hpp"
+#include "Scene.hpp"
 
 Renderer renderer;
 Loader loader;
 Scene scene;
 
-int render = 0;
-int sceneInfo = 0;
-int pixelRay = 0;
-int firstHit = 0;
+/* Flags for different arguments
+ 0 - render
+ 1 - sceneinfo
+ 2 - pixelray
+ 3 - firsthit
+ 4 - pixelcolor
+*/
+int arg_flags[4] = {0};
 
 using namespace std;
 
 int main(int args, char **argv) {
 	
-	// Catch command line arguments
-	render = !strcmp(argv[1], "render");
-	sceneInfo = !strcmp(argv[1], "sceneinfo");
-	pixelRay = !strcmp(argv[1], "pixelray");
-	firstHit = !strcmp(argv[1], "firsthit");
-	if (!render && !sceneInfo && !pixelRay && !firstHit) {
-		cerr << "Invalid arguments" << endl;
+	// Catch empty line args
+	if (args == 1) {
 		return 1;
-	}	
+	}
+
+	// Store command line arguments
+	arg_flags[0] = !strcmp(argv[1], "render");
+	arg_flags[1] = !strcmp(argv[1], "sceneinfo");
+	arg_flags[2] = !strcmp(argv[1], "pixelray");
+	arg_flags[3] = !strcmp(argv[1], "firsthit");
+	arg_flags[4] = !strcmp(argv[1], "pixelcolor");
+	renderer.BRDF = !strcmp(argv[args-1], "-altbrdf");
 
 	cout << setprecision(4);
 
 	// Parse file + create scene
 	loader.parse(argv[2], scene);
 
-	if (sceneInfo) {
+	// Sceneinfo
+	if (arg_flags[1]) {
 		scene.print();
 	}
-	if (pixelRay || firstHit) {
+
+	// Pixelray 
+	if (arg_flags[2] || arg_flags[3]) {
 		int window_width = atoi(argv[3]);
 		int window_height = atoi(argv[4]);
 		int pixel_x = atoi(argv[5]);
@@ -46,15 +56,18 @@ int main(int args, char **argv) {
 		Ray ray = scene.createCameraRay(window_width, window_height, pixel_x, pixel_y);
 		cout << "Pixel: [" << pixel_x << ", " << pixel_y << "] ";
 		ray.print();
-		if (firstHit) {
+		// Firsthit
+		if (arg_flags[3]) {
 			Intersection in = scene.findIntersection(ray);
 			in.print();
 		}
  	}
-	if (render) {
+
+ 	// Render
+	if (arg_flags[0]) {
 		int window_width = atoi(argv[3]);
 		int window_height = atoi(argv[4]);
 		renderer.render(scene, window_width, window_height);
 	}
 	return 0;
-}
+	}
