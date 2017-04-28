42bcc9c983432f0b198689ac4ce2de7518e014d9
Jaafer Sheriff <jsheriff@calpoly.edu>
2017-04-27 20:32:35 -0700

    More make fixes

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -11,7 +11,6 @@ Camera::Camera() {
 }
 
 void Camera::print() {
-	cout << setprecision(4);
 	cout << "Camera:" << endl;
 	cout << "- Location: {";
 		cout << location.x << " " << location.y << " " << location.z;
